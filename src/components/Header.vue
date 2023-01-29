132b3604ed9ca81a2b8c7ddfec309fe6e88bc004
Ranecodes <ranehobasi@gmail.com>
2023-01-29 16:35:37 +0100

    btn click added

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,32 +1,36 @@
 <template>
-    <header>
-        <h1>{{ title }}</h1>
-        <Button @toggle-add-task="$emit('toggle-add-task')" :text="showAddTask ? 'Close':'Add Task'" :color="showAddTask ? 'red' : 'green'" ></Button>
-    </header>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button
+      @btn-click="$emit('toggle-add-task')"
+      :text="showAddTask ? 'Close' : 'Add Task'"
+      :color="showAddTask ? 'red' : 'green'"
+    ></Button>
+  </header>
 </template>
 
 <script>
-import Button from './Button.vue';
+import Button from "./Button.vue";
 
 export default {
-    name: 'Header',
-    props: {
-        title: {
-            type: String,
-            showAddTask: Boolean,
-        }
+  name: "Header",
+  props: {
+    title: {
+      type: String,
+      showAddTask: Boolean,
     },
-    components: {
-        Button,
-    },
-}
+  },
+  components: {
+    Button,
+  },
+};
 </script>
 
 <style scoped>
 header {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin-bottom: 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
 }
 </style>
